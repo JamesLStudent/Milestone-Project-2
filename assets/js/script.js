fa2f4a224f6f4455e48efa70c903a53d80399ef5
JamesLStudent <jamesleonard193@googlemail.com>
2021-10-19 20:37:49 +0000

    test code to ensure win, lose and draw functions work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 let computerResult = "";
 function roll(event){
     event.preventDefault();
-    computerResult = Math.floor(Math.random()*5); //Generates a number between 0-4
-    console.log(computerResult);
+    computerResult = Math.floor(Math.random()*5); //Generates a number 0-4
+    console.log('computer value: '+computerResult);
     playerAnswer();
 }
 let gameForm = document.getElementsByTagName('input');
@@ -14,22 +14,28 @@ caused <label>s to fire the same event as <input>s upon
 submit, causing two events where only one was warranted.
 This bug was solved thanks to users on Stack Overflow,
 linked in the README */
+
 let result = "";
 function playerAnswer(){
     if(document.getElementById('rock').checked) {
         result = 0;
+        console.log('player value: ' + result);
         checkResults()
     } else if(document.getElementById('paper').checked) {
         result = 1;
+        console.log('player value: ' + result);
         checkResults()
     } else if(document.getElementById('scissors').checked) {
         result = 2;
+        console.log('player value: ' + result);
         checkResults()
     } else if(document.getElementById('lizard').checked) {
         result = 3;
+        console.log('player value: ' + result);
         checkResults()
     } else if(document.getElementById('spock').checked) {
         result = 4;
+        console.log('player value: ' + result);
         checkResults()
     }
 }
@@ -37,18 +43,17 @@ function playerAnswer(){
 /* The above code checks the player's selection and responds
 accordingly.
 */
-let divResult = document.getElementById('results-box').innerHTML; //finds an empty <div>
 
 function win(){
-    divResult = "Win!"; //Changes the <div>'s content to say Win!
+    document.getElementById('results-box').innerHTML = "Win!"; //Changes the <div>'s content to say Win!
 }
 
 function lose(){
-    divResult = "Lose!"; //Changes the <div>'s content to say Lose!
+    document.getElementById('results-box').innerHTML = "Lose!"; //Changes the <div>'s content to say Lose!
 }
 
 function draw(){
-    divResult = "Draw!"; //Changes the <div>'s content to say Draw!
+    document.getElementById('results-box').innerHTML = "Draw!"; //Changes the <div>'s content to say Draw!
 }
 
 function checkResults(){
